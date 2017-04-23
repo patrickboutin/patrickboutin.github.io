c74d6a59e196c64af753d34b6de3b8126e671208
Patrick Connolly-Boutin <Patrickc@consortech.com>
2017-04-23 10:51:40 -0400

    set default values for variables

--- minimalist.js
+++ minimalist.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
     var unit = $(".inputUnit").val();
     console.log(unit);
+    var weight = $(".inputweight").val();
+    console.log(weight);
+    var medication = $(".inputMedication").val();
+    console.log(medication);
+    
     $( ".inputweight" ).change(function() {
         var weight = $(".inputweight").val();
         console.log( weight );
