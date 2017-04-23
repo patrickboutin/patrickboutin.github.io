d9ce1c47a2ca6058969c91e2341892f0021dad93
Patrick Connolly-Boutin <Patrickc@consortech.com>
2017-04-23 10:38:59 -0400

    unit & medication variables

--- minimalist.js
+++ minimalist.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
     $( ".inputweight" ).change(function() {
         var weight = $(".inputweight").val();
-        alert( weight );
+        console.log( weight );
+    });
+    $( ".inputUnit" ).change(function() {
+        var unit = $(".inputUnit").val();
+        console.log(unit);
+    });
+    $( ".inputMedication" ).change(function() {
+        var medication = $(".inputMedication").val();
+        console.log(medication);
     });
 });
