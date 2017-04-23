3db4b519e11c927f3e9c323126a9be8b720c51ab
Patrick Connolly-Boutin <Patrickc@consortech.com>
2017-04-23 15:15:26 -0400

    number variable tests

--- minimalist.js
+++ minimalist.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
-    var unit = $(".inputUnit").val();
-    var weight = $(".inputweight").val();
-    var medication = $(".inputMedication").val();
+    var unit: number = $(".inputUnit").val();
+    var weight: number = $(".inputweight").val();
+    var medication: number = $(".inputMedication").val();
 
     $( ".inputweight" ).change(function() {
-        var weight = $(".inputweight").val();
+        var weight: number = $(".inputweight").val();
     });
     $( ".inputUnit" ).change(function() {
-        var unit = $(".inputUnit").val();
+        var unit: number = $(".inputUnit").val();
     });
     $( ".inputMedication" ).change(function() {
-        var medication = $(".inputMedication").val();
+        var medication: number = $(".inputMedication").val();
     });
-    var result = parseFloat(weight) * parseFloat(unit) * parseFloat(medication);
+    var result: number = weight * unit * medication;
     console.log(result);
     $(".bg-success").text("Dose à donner: " + result + " ml");
 });
