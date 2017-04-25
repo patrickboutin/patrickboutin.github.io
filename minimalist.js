28cbe6424b5c2fc03c60c9598f2569099f937dee
Patrick Connolly-Boutin <Patrickc@consortech.com>
2017-04-24 21:59:18 -0400

    2

--- minimalist.js
+++ minimalist.js
@@ -5,12 +5,19 @@ $(document).ready(function() {
 
     $( ".inputweight" ).change(function() {
         var weight = $(".inputweight").val();
+        var result = parseFloat(weight) * parseFloat(unit) * parseFloat(medication);
+        $(".bg-success").text("Dose à donner: " + result + " ml");
+
     });
     $( ".inputUnit" ).change(function() {
         var unit = $(".inputUnit").val();
+        var result = parseFloat(weight) * parseFloat(unit) * parseFloat(medication);
+        $(".bg-success").text("Dose à donner: " + result + " ml");
     });
     $( ".inputMedication" ).change(function() {
         var medication = $(".inputMedication").val();
+        var result = parseFloat(weight) * parseFloat(unit) * parseFloat(medication);
+        $(".bg-success").text("Dose à donner: " + result + " ml");
     });
     var result = parseFloat(weight) * parseFloat(unit) * parseFloat(medication);
     console.log(result);
