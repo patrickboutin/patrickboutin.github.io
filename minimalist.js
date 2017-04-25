272a0500630d2ba5decd195ae529176366220a2d
Patrick Connolly-Boutin <Patrickc@consortech.com>
2017-04-24 22:09:55 -0400

    Changed js function variables

--- minimalist.js
+++ minimalist.js
@@ -1,23 +1,12 @@
 $(document).ready(function() {
-    var unit = $(".inputUnit").val();
-    var weight = $(".inputweight").val();
-    var medication = $(".inputMedication").val();
-
-    $( ".inputweight" ).change(function() {
-        var weight = $(".inputweight").val();
-        var result = parseFloat(weight) * parseFloat(unit) * parseFloat(medication);
-        $(".bg-success").text("Dose à donner: " + result + " ml");
-
+    var weight = $(".inputweight").change(function() {
+        $(".inputweight").val();
     });
-    $( ".inputUnit" ).change(function() {
-        var unit = $(".inputUnit").val();
-        var result = parseFloat(weight) * parseFloat(unit) * parseFloat(medication);
-        $(".bg-success").text("Dose à donner: " + result + " ml");
-    });
-    $( ".inputMedication" ).change(function() {
-        var medication = $(".inputMedication").val();
-        var result = parseFloat(weight) * parseFloat(unit) * parseFloat(medication);
-        $(".bg-success").text("Dose à donner: " + result + " ml");
+    var unit = $(".inputUnit").change(function() {
+    	$(".inputUnit").val();
+      });
+    var medication = $(".inputMedication").change(function() {
+        $(".inputMedication").val();
     });
     var result = parseFloat(weight) * parseFloat(unit) * parseFloat(medication);
     console.log(result);
